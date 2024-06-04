ac780e3265d73ce267b7bb4ee0eb36b373edf0fe
nicemozg <nice.mozg@gmail.com>
2024-06-04 23:09:46 +0500

    Add auth_provider to users

--- database/schema/01_create_users_table.sql
+++ database/schema/01_create_users_table.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     email TEXT UNIQUE NOT NULL,
     username TEXT UNIQUE,
+    auth_provider VARCHAR(55) DEFAULT 'forum',
     password_hash TEXT NOT NULL
 );
