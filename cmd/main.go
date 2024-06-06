03f88af6524df4d156319d7af91b07a002b92264
nicemozg <nice.mozg@gmail.com>
2024-06-07 00:53:56 +0500

    Update README.md

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"forum/internal/app"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
+	// Открытие файла в режиме добавления или создание нового файла, если он не существует
+	file, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal("Cannot open log file:", err)
+	}
+	defer file.Close()
+
+	// Установка многопоточного вывода в файл и в консоль
+	multiWriter := io.MultiWriter(file, os.Stdout)
+	log.SetOutput(multiWriter)
+
+	// Логирование сообщений
+	log.Println("Программа запущена")
+	log.Printf("Значение переменной: %d", 42)
+
+	// Запуск приложения после настройки логирования
 	app.Run()
 }
--- go.mod
+++ go.mod
@@ -8,4 +8,4 @@ require (
 	golang.org/x/crypto v0.22.0
 )
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
